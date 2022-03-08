e05d8975e0b085b93bab5d86ad01c2a47ec4a154
AnthonyAFK <77146108+Anthonylucaslopes@users.noreply.github.com>
2022-03-08 10:01:04 +0100

    Add files via upload

---
+++ ajouter_utilisateur.php
@@ -0,0 +1,25 @@
+<?php
+include('template.php');
+
+if( !empty($_GET['identifiant']) && !empty($_GET['mot_de_passe']) && !empty($_GET['Type']) )
+{
+
+	$requete = $pdo->prepare("INSERT INTO `Utilisateur` (`id`, `identifiant`, `mot_de_passe`, `Type`);");
+	$requete->bindParam(':identifiant', $_GET['identifiant']);
+	$requete->bindParam(':mot_de_passe', $_GET['mot_de_passe']);
+	$requete->bindParam(':Type', $_GET['Type']);
+
+
+	if( $requete->execute() ){
+		$success = true;
+		$msg = 'Le compte a été ajouté';
+							}
+else {
+		$msg = "Une erreur s'est produite";
+	 }
+} else {
+	$msg = "Il manque des informations";
+}
+
+reponse_json($success, $data, $msg);
+
