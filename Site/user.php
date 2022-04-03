dcfc934789d081fd91232f9d9f02ddc344fb8aae
Ewixam <74783098+Ewixam@users.noreply.github.com>
2022-04-03 17:26:14 +0200

    ✅User plate data

--- Site/user.php
+++ Site/user.php
@@ -23,6 +23,31 @@
         <div class="plate-list">
             <h2>MES PLAQUES </h2>
             <table>
+                <?php
+
+                $curl = curl_init();
+                $username = 'djboss';
+
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => '%7B%7Bmock%7D%7D/utilisateur?username=' . $username,
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'GET',
+                    CURLOPT_HTTPHEADER => array(
+                        'Accept: application/json'
+                    ),
+                ));
+
+                $response = curl_exec($curl);
+
+                curl_close($curl);
+                echo test;
+
+                ?>
                 <tr>
                     <td class="plate-number">AA-123-BB</td>
                     <td>
