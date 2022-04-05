3d3cdd95424912dff68979408cc55a386a075f3c
AnthonyAFK <77146108+Anthonylucaslopes@users.noreply.github.com>
2022-04-05 14:14:11 +0200

    Add files via upload

---
+++ ajouter_plaque_3.0.php
@@ -0,0 +1,23 @@
+<?php
+
+require "bdd.php";
+$bdd = pdo();
+
+if (isset($_GET['plaque'],$_GET['proprietaire']))
+{
+    $query = "INSERT INTO easyportal.plates (`plateNumber`, `owner`) VALUES (:plate,:owner)";
+    $stmt = $bdd->prepare($query);
+    $stmt->execute(array('plate'=>$_GET['plaque'],'owner'=>$_GET['proprio']));
+    if($stmt->errorCode()=="00000"){
+        $rep = array("success" => true);
+        $rep += array("message" => "Plaque ajouter");
+    }else{
+        $rep = array("success" => false);
+        $rep += array("message" => $stmt->errorInfo());
+    }
+    echo(json_encode($rep));
+}else{
+    $rep = array("success" => false);
+    $rep += array("message" => "Parametre manquant");
+    echo(json_encode($rep));
+}
