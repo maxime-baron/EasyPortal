ba091768e9302ed882274ee85f904266884b1bac
AnthonyAFK <77146108+Anthonylucaslopes@users.noreply.github.com>
2022-04-06 17:57:52 +0200

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -1,14 +1,29 @@
 <?php
-$hote = 'localhost';
-$nom_bdd = 'easyportal';  //mettre nom bdd
-$utilisateur = 'root'; 
-$mot_de_passe ='';
+//header('Content-Type: application/json; charset=utf-8');
+//function pdo(){
+//
+//    $hote = '51.210.151.13:3306';
+//    $nom_bdd = 'easyportal';  //mettre nom bdd
+//    $utilisateur = 'easyportal';
+//    $mot_de_passe ='easyportal';
+//
+//    try {
+//        $bdd = new PDO("mysql:host={$hote};dbname={$nom_bdd}","{$utilisateur}","{$mot_de_passe}");  // test connexion
+//        return $bdd;
+//    } catch(Exception $e) {
+//        die('Erreur :'.$e->getMessage());
+//    }
+//}
+$host="51.210.151.13";
+$port=3306;
+$socket="";
+$user="easyportal";
+$password="easyportal";
+$dbname="easyportal";
 
-try {
-    $pdo = new PDO('mysql:host='.$hote.';port='.$nom_bdd, $utilisateur, $mot_de_passe);  // test connexion
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+or die ('Could not connect to the database server' . mysqli_connect_error());
 
-} catch(Exception $e) {
-	reponse_json($success, $data, 'Echec de la connexion à la base de données '); // connexion non établie
-    exit();
+//$con->close();
 
-}
+?>
