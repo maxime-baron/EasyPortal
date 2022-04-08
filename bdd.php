bda761b92182711b573528fd831df17c578dc506
AnthonyAFK <77146108+Anthonylucaslopes@users.noreply.github.com>
2022-04-08 11:04:40 +0200

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -1,7 +1,5 @@
 <?php
-//header('Content-Type: application/json; charset=utf-8');
-//function pdo(){
-//
+
 //    $hote = '51.210.151.13:3306';
 //    $nom_bdd = 'easyportal';  //mettre nom bdd
 //    $utilisateur = 'easyportal';
@@ -9,11 +7,11 @@
 //
 //    try {
 //        $bdd = new PDO("mysql:host={$hote};dbname={$nom_bdd}","{$utilisateur}","{$mot_de_passe}");  // test connexion
-//        return $bdd;
+//        echo "creusii";
 //    } catch(Exception $e) {
-//        die('Erreur :'.$e->getMessage());
+//        die('Erreur :' . $e->getMessage());
 //    }
-//}
+
 $host="51.210.151.13";
 $port=3306;
 $socket="";
@@ -21,9 +19,7 @@ $user="easyportal";
 $password="easyportal";
 $dbname="easyportal";
 
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+$bdd = new mysqli($host, $user, $password, $dbname, $port, $socket)
 or die ('Could not connect to the database server' . mysqli_connect_error());
 
 //$con->close();
-
-?>
